3a50e4c4b9b628669860b1e605fe1e7fe6342c0b
Sair Ali <sairali7037@gmail.com>
2026-03-20 16:41:46 +0530

    adding home page

--- backend/health_backend/urls.py
+++ backend/health_backend/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("🚀 IoT Healthcare Backend is LIVE!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('sensors.urls')),
+    path('', home),
 ]
